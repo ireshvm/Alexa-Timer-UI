a91736946620e45b4d4024d63b4a2c4252706fa0
ireshvm <39979136+ireshvm@users.noreply.github.com>
2021-04-12 11:20:13 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,49 @@
+let hours = 1; // starting number of hours
+let minutes = 1; // starting number of minutes
+let seconds = 5; // starting number of seconds
+
+// converts all to seconds
+let totalSeconds = hours * 60 * 60 + minutes * 60 + seconds;
+
+//temporary seconds holder
+let tempSeconds = totalSeconds;
+
+// calculates number of  hours, minutes and seconds from a given number of seconds
+const convert = (value, inSeconds) => {
+if (value > inSeconds) {
+    let x = value % inSeconds;
+    tempSeconds = x;
+    return (value - x) / inSeconds;
+} else {
+    return 0;
+}
+};
+
+//sets seconds
+const setSeconds = (s) => {
+document.querySelector("#seconds").textContent = s + "s";
+};
+
+//sets minutes
+const setMinutes = (m) => {
+document.querySelector("#minutes").textContent = m + "m";
+};
+
+//sets hours
+const setHours = (h) => {
+document.querySelector("#hours").textContent = h + "h";
+};
+
+// Update the count down every 1 second
+var x = setInterval(() => {
+//clears countdown when all seconds are counted
+if (totalSeconds <= 0) {
+    clearInterval(x);
+}
+
+setHours(convert(tempSeconds, 60 * 60));
+setMinutes(convert(tempSeconds, 60));
+setSeconds(tempSeconds == 60 ? 59 : tempSeconds);
+totalSeconds--;
+tempSeconds = totalSeconds;
+}, 1000);
